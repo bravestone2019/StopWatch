d6f3788cb16c205619d38ee95259ddd04088dce0
Animesh dung dung <animeshdungdung9@gmail.com>
2024-08-03 23:42:30 +0530

    seting up the project stopwatch

---
+++ src/Component/Stopwatch.jsx
@@ -0,0 +1,12 @@
+function Stopwatch() {
+return(
+    <>
+    <h1>StopWatch</h1>
+    <h2>Time :</h2>
+    <button>Start</button>
+    <button>Reset</button>
+    </>
+);
+};
+
+export default Stopwatch;
